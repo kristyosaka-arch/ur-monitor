23ba4176b811efe85f1f86cb94bf3e39f787d1ec
kristyosaka-arch <kristyosaka@gmail.com>
2026-06-03 14:35:31 +0900

    Update main.py

--- main.py
+++ main.py
@@ -27,6 +27,9 @@ DANCHI = {
     
     "川崎日進":
         "https://www.ur-net.go.jp/chintai/kanto/kanagawa/40_1830.html",
+    
+    "サンスクエア川崎":
+        "https://www.ur-net.go.jp/chintai/kanto/kanagawa/40_2190.html",
 
     "龟户二丁目":
         "https://www.ur-net.go.jp/chintai/kanto/tokyo/20_1660.html",
